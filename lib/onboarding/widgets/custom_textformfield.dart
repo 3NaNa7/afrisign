d3d0f3a268cda733080b2ce0b06f59145e17f559
3NaNa7 <samuelsonacheampong@gmail.com>
2024-06-28 17:04:25 +0000

    add an option to set obsureText for input fields

--- lib/onboarding/widgets/custom_textformfield.dart
+++ lib/onboarding/widgets/custom_textformfield.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
   final String label;
+  final bool obscureText;
 
-  const CustomTextFormField({super.key, required this.label});
+  const CustomTextFormField(
+      {super.key, required this.label, this.obscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 20),
       child: TextFormField(
+        obscureText: obscureText,
         decoration: InputDecoration(
             label:
                 Text(label, style: Theme.of(context).textTheme.displayMedium),
