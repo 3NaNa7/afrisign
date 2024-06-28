1229553951c417862779244d9593ea94078f065a
3NaNa7 <samuelsonacheampong@gmail.com>
2024-06-28 13:35:26 +0000

    set up auth_screen

--- lib/onboarding/screens/auth_screen.dart
+++ lib/onboarding/screens/auth_screen.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
+
+  final String boldText = 'An Interactive Sign Language to Text App';
+  final String description =
+      'This app is designed to bridge the communication gap between you and and an ear-impaired person. Seamlessly translates sign language to text.';
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +21,65 @@ class AuthScreen extends StatelessWidget {
         )),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: ,
+          body: Container(
+            margin: EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Card(
+                  color: Color.fromRGBO(255, 255, 255, 0.2),
+                  child: Padding(
+                    padding: const EdgeInsets.all(11.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          boldText,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(color: Colors.white),
+                        ),
+                        Text(description,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(color: Colors.white))
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(111, 214, 149, 1)),
+                    onPressed: () {},
+                    child: Text('Sign Up Now!',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge!
+                            .copyWith(color: Colors.white)),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(color: Colors.white),
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text('Sign In',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15)))
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
