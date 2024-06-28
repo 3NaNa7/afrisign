5850bd37d2e4d209db2f5f7a26e16963f6c52252
3NaNa7 <samuelsonacheampong@gmail.com>
2024-06-28 16:26:23 +0000

    custom_textformfield accepts a label property

--- lib/onboarding/widgets/custom_textformfield.dart
+++ lib/onboarding/widgets/custom_textformfield.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+  final String label;
+
+  const CustomTextFormField({super.key, required this.label});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: TextFormField(
+        decoration: InputDecoration(
+            label:
+                Text(label, style: Theme.of(context).textTheme.displayMedium),
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(10))),
+      ),
+    );
   }
 }
